c1cafffef657eb1ec3f80eafdebb18ade24fbf36
monteon <joel@monteon.mx>
2025-05-07 16:17:36 -0600

    Added cover images to blog posts

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,9 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
-  resources :blog_posts
+  resources :blog_posts do
+    resource :cover_image, only: [ :destroy ], module: :blog_posts
+  end
 
   # Defines the root path route ("/")
   root "blog_posts#index"
